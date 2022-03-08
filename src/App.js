43196e4780e8087876933dae1864a85492908e53
anchenayres <91013859+anchenayres@users.noreply.github.com>
2022-03-08 22:40:21 +0200

    Implemented React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navigation from './components/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation>
+        
+      </Navigation>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+const Navigation = () => {
+    return (
+        <>
+        <div className = "nav">
+            <h1>Open Space</h1>
+        <ul>
+            <li><a href="/">Component One</a></li>
+            <li><a href="/ComponentTwo">Component Two</a></li>
+            <li><a href="/ComponentThree">Component Three</a></li>
+        </ul>
+        </div>
+        </>
+    )
+}
+
+export default Navigation; 
