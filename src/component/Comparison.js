b3f1c3870d96f18fdd211feaf865ffad395675e8
anchenayres <91013859+anchenayres@users.noreply.github.com>
2022-03-28 23:07:20 +0200

    Testing
    Testing the filter

--- src/component/Comparison.js
+++ src/component/Comparison.js
@@ -1,59 +1,107 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "chart.js/auto";
-import {Bar, Pie, PolarArea} from "react-chartjs-2";
+import axios from "axios";
+import { useState, useEffect, useRef } from 'react';
+import { Line, Pie, PolarArea } from 'react-chartjs-2';
+import TableItem from "./TableItem";
 
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Comparison = () =>{
 
-    return (
-        <>
-        <h>Exploring Planets</h>
-        <div className="select">
-            <select>
-                <option value="0">All</option>
-                <option value="1">Pie Chart</option>
-                <option value="3">Bar Graph</option>
-                <option value="4">Polar Radar Graph</option>
-                <option value="5">Timeline Graph</option>
-                
-            </select>
-        </div>
-        <div className="select-results">
-        </div>
-        
-        <div className="bargraph">
-            <Bar data= {{
-        labels: ['La Lune', 'Phobos', 'Callisto', 'Himalia', 'Praxidike', 'Europa'],
-        datasets: [{
-            label: 'Density (g.cm3)',
-            data: [3.34400, 1.90000, 1.83000, 1.00000, 1.00000, 3.01000],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-            }} height={400} width={600} option={{}}
-            />
-        </div>
-        <div className="piegraph">
-        <Pie data= {{
-        labels: ['Planet', 'Dwarf Planet', 'Moons Planet', 'Moons Dwarf Planet', 'Moons Asteroid'],
+    const [graphInfo, setgraphInfo] = useState([]);
+    const [timeInfo, settimeInfo] = useState([]);
+    const [showInfo, setFailedInfo] = useState ([]);
+   
+
+    const labels = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'];
+
+
+    useEffect(()=> {
+        axios.get('https://api.le-systeme-solaire.net/rest/bodies/')
+        .then((res) => {
+            console.log(res);
+            const data = res.data.bodies
+           
+
+            const yearlyDisc = [];
+
+            for (let i = 0; i < data.length; i++) {
+                let date = data[i].discoveryDate;
+                date = date.substring(6);
+                if (date === "2010") {
+                    yearlyDisc.push("2010");  
+                }
+                if (date === "2011") {
+                    yearlyDisc.push("2011");  
+                }
+                if (date === "2012") {
+                    yearlyDisc.push("2012");  
+                }
+                if (date === "2013") {
+                    yearlyDisc.push("2013");  
+                }
+                if (date === "2014") {
+                    yearlyDisc.push("2014");  
+                }
+                if (date === "2015") {
+                    yearlyDisc.push("2015");  
+                }
+                if (date === "2016") {
+                    yearlyDisc.push("2016");  
+                }
+                if (date === "2017") {
+                    yearlyDisc.push("2017");  
+                }
+                if (date === "2018") {
+                    yearlyDisc.push("2018");  
+                }
+                if (date === "2019") {
+                    yearlyDisc.push("2019");  
+                }
+                if (date === "2020") {
+                    yearlyDisc.push("2020");  
+                }
+            }
+          
+            console.log(yearlyDisc);
+
+            const ten = yearlyDisc.filter((item) => item === "2010").length;
+            const eleven = yearlyDisc.filter((item) => item === "2011").length;
+            const twelve = yearlyDisc.filter((item) => item === "2012").length;
+            const thirteen = yearlyDisc.filter((item) => item === "2013").length;
+            const fourteen = yearlyDisc.filter((item) => item === "2014").length;
+            const fifteen = yearlyDisc.filter((item) => item === "2015").length;
+            const sixteen = yearlyDisc.filter((item) => item === "2016").length;
+            const seventeen = yearlyDisc.filter((item) => item === "2017").length;
+            const eighteen = yearlyDisc.filter((item) => item === "2018").length;
+            const nineteen = yearlyDisc.filter((item) => item === "2018").length;
+            const twenty = yearlyDisc.filter((item) => item === "2018").length;
+
+            console.log(ten);
+            console.log(twenty);
+
+            settimeInfo([ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen, twenty]);
+            const moon = data.filter((item) => item.bodyType === "Moon").length;
+            const planet = data.filter((item) => item.bodyType === "Planet").length;
+            const asteroid = data.filter((item) => item.bodyType === "Asteroid").length;
+            const comet = data.filter((item) => item.bodyType === "Comet").length;
+
+            setgraphInfo([moon, planet, asteroid, comet]);
+
+            
+
+            
+        })
+    }, [])
+
+    const showItems = showInfo.filter((item) => <TableItem id)
+
+    const ChartData = {
+        labels: ['moon', 'planet', 'asteroid', 'comet'],
         datasets: [{
             label: '# of Votes',
-            data: [8, 5, 206, 9, 558],
+            data: graphInfo,
             backgroundColor: [
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
@@ -70,19 +118,59 @@ const Comparison = () =>{
             ],
             borderWidth: 1
         }]
-            }} height={400} width={600} option={{}}
+    }
             
-            />
+    function updatedTime() {
+        if(getValue === "Failed") {
+            settimeInfo(failedItems);
+        } else if (getValue === "Success") {
+            settimeInfo(successItems);
+        }
+    }
             
+
+
+    
+    return (
+        <>
+        <div className="left-panel"> 
+            <Line data={ChartData} />
         </div>
+    
+
+
+        <h>Exploring Planets</h>
+       
+        <div className="select">
+            <select className="chartType" id="chartTpe" onchange="updateChartType()">
+                <option value="pie">Pie Chart</option>
+                <option value="polar">Polar Radar Graph</option>
+                <option value="line">Timeline Graph</option>
+            </select>
+        </div>
+
+        <div className="select-results">
+            <div id="mychart"></div>
+        </div>
+
+    
+
+
+
+
+        
+
+
+        
+        
         
         <div className="areachart">
-            <PolarArea data= {{
-        labels: ['La Lune', 'Phobos', 'Callisto', 'Himalia', 'Praxidike', 'Europa'],
+        <PolarArea data= {{
+        labels: ['moon', 'planet', 'asteroid', 'comet'],
         datasets: [
             {
               label: '# of Votes',
-              data: [7.34600, 1.06000, 1.07590, 9.50000, 4.30000, 4.80000],
+              data: graphInfo,
               backgroundColor: [
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
@@ -102,11 +190,46 @@ const Comparison = () =>{
           ],
             }} height={400} width={600} option={{}}
             
-            />
-            
+            />            
         </div>
+
+
+        <Line data = {{
+                    labels,
+                    datasets: [
+                        {
+                        label: ['timeline'],
+                        data: timeInfo,
+                        borderColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(223, 119, 255)',
+                            'rgb(235, 255, 119)',
+                            'rgb(142, 255, 119)',
+                            'rgb(255, 119, 248)',
+                            'rgb(223, 119, 255)',
+                            'rgb(235, 255, 119)',
+                            'rgb(142, 255, 119)',
+                            'rgb(255, 119, 248)'
+                        ],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.5)',
+                            'rgba(223, 119, 255, 0.5)',
+                            'rgba(235, 255, 119, 0.5)',
+                            'rgba(142, 255, 119, 0.5)',
+                            'rgba(255, 119, 248, 0.5)',
+                            'rgba(223, 119, 255, 0.5)',
+                            'rgba(235, 255, 119, 0.5)',
+                            'rgba(142, 255, 119, 0.5)',
+                            'rgba(255, 119, 248, 0.5)'
+                        ]
+                        
+                        }, 
+                    ],
+                }} 
+                />
         </>
-    )
+    
+    );
     
 }
 
--- src/component/TableItem.js
+++ src/component/TableItem.js
@@ -1,40 +1,38 @@
 import React from "react";
+import { Line } from "react-chartjs-2";
 
 
 const TableItem = (props) => {
 
 
     return (
-      <div className="tableItem" key={props.id}> 
-          <div className="bargraph">
-            <Bar data= {{
-        labels: ['La Lune', 'Phobos', 'Callisto', 'Himalia', 'Praxidike', 'Europa'],
-        datasets: [{
-            label: 'Density (g.cm3)',
-            data: [3.34400, 1.90000, 1.83000, 1.00000, 1.00000, 3.01000],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-            }} height={400} width={600} option={{}}
-            />
-        </div>           
+        <>
+       <div className="tableItem" key={props.data}>
+          <img src={props.imgUrl} />   
+          <p>Flight Name: {props.fname}</p>     
+          <p>Flight Number: {props.fnum}</p>  
+          <p><a target="_blank" href={newLink}>Watch the Launch</a></p>
       </div>
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </>          
+      
     );
   }
-  
+
   export default TableItem;
+
+
+
+  
+ 
