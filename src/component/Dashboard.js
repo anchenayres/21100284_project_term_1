36e00b5061cd7ea45e87743dba12efc24c06d25f
anchenayres <91013859+anchenayres@users.noreply.github.com>
2022-03-30 10:41:18 +0200

    Quick
    quickly added bar graph to dashboard. Wasn't final draft, was a first draft

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -130,7 +130,36 @@ const Dashboard = () =>{
                 />
             </div>
         </div>
-        <div className="bar_block">This is where the bar graph will be</div>
+        <div className="bar_block">
+                <div className="dash-bar">
+                <h5>Bar Chart Discovery Total</h5>
+            <Bar data= {{
+        labels: ['Gauteng', 'Freestate', 'Limpopo', 'Eastern Cape', 'Kwazulu-Natal', 'Northern Cape'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    }} height={400} width={600} option={{}}
+            />
+        </div>
+        </div>
         <div className="pie_block">
         <div className="dash-pie-graph">
             <Pie data= {{
